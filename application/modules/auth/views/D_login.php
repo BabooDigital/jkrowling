ab0802869d0905c7d856e28759dee04b064f943b
Aditia Nugraha <aditia@baboo.id>
2017-10-11 19:13:45 +0700

    View untuk Login

--- application/modules/auth/views/D_login.php
+++ application/modules/auth/views/D_login.php
@@ -6,207 +6,170 @@
 </head>
 <!-- CSS -->
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/baboo.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/page/login.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/animate.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/simple-line-icons.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/fonts/fonts.css">
 <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<!-- BODY -->
-<body style="background-color: #fff;">
+<body>
 	<div class="container-fluid">
-		<div class="row">
+		<div class="row no-gutters">
+				<div class="col-lg-8 col-xl-9 nopadding">
+		 			<div class="left-side">
+		 				<center>
+		 					<img src="<?php echo base_url();?>public/img/logo_purple.png" class="img-fluid img-left animated slideInDown center-content">
+		 					<p class="text-img animated slideInDown">Selamat datang di Baboo</p>
+		 				</center>
+		 			</div>
+		 		</div>
 
-			<!-- LEFT SIDE -->
-			<div class="col-md-6 col-lg-7 col-xl-8 padding-0">
-				<div class="left-side hidden-md-down">
-					<div class="side-content-l">
-						<center>
-							<img src="<?php echo base_url();?>public/img/logo_purple.png" class="img-logo-login img-fluid animated slideInUp" style="">
-							<p class="welcome-message  animated slideInLeft" style="animation-duration: 2s;">Selamat  datang di baboo</p>
-						</center>
-					</div>
-				</div>				
-			</div>
-			<!-- END LEFT SIDE -->
+				<div class="col-lg-4 col-xl-3 nopadding">
+		 			<div class="right-side">
+		 				<div class="right-side-p">
+		 						<div class="row">
+		 							<div class="col-lg-12">
+		 								<a href="#" class="skip-text">Langsung Baca Buku</a>
+		 							</div>
 
-			<!-- RIGHT SIDE -->
-			<div class="col-md-6 col-lg-5 col-xl-4 col-xs-12 col-sm-12 padding-0">
-				<div class="right-side  hidden-md-down">
-					<div class="side-content-r">
-						<a class="pull-right register-link2" href="#">Langsung baca buku <i class="fa fa-arrow-right icon-langsung-baca" aria-hidden="true"></i></a>	
-							<div style="margin-top:100px;">
-								<p class="text-label-login">Lanjutkan dengan</p>
-									<div class="social-login  hidden-xs-down hidden-sm-down">
-										<div class="row">
-											<div class="col-md-6">
-												<button class="btn btn-facebook btn-block">
-													<img src="<?php echo base_url();?>public/img/assets/combined-shape-copy.svg" class="Combined-Shape-Copy">
-														<span class="btn-teks">Facebook</span>
-												</button>
-											</div>
-											<div class="col-md-6">
-												<button class="btn btn-google btn-block">
-													<img src="<?php echo base_url();?>public/img/google-button.png" class="img-fluid img-button">
-														<span class="btn-teks">Google</span>
-												</button>
-											</div>
-										</div>
-									</div>
-							</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<p class="text-label-login">Atau login dengan email</p>
+		 							<div class="col-lg-12">
+		 								<p class="right-text" style="margin-top:72.2px;">Lanjutkan dengan</p>
+		 							</div>
 
-								<!-- FORM -->
-								<form>
-									<div class="form-group">
-										<input oninvalid="this.setCustomValidity('Silahkan masukan email dengan benar, contoh : andi@baboo.id')" type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
-									</div>
-									<div class="form-group">
-										<input type="password" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Password">
-									</div>
-										<p class="text-right register-link" style="margin-top:30px;">Belum punya akun? <a data-toggle="modal" data-target="#exampleModal" href="#" class="register-link2">Daftar disini</a></p>
-										<button class="btn button-login pull-right" style="margin-top:10px;"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
-								</form>
-								<!-- END FORM -->
+		 							<div class="col-lg-6">
+		 								<button class="btn btn-block btn-sosmed">
+											<img src="public/img/assets/fb-icon.svg" class="btn-img-sosmed"> <span class="btn-text-sosmed">Facebook</span>
+										</button>
+		 							</div>
 
-							</div>
-						</div>
-					</div>	
-				</div>
-			</div>
-			<!-- END RIGHT SIDE -->
+		 							<div class="col-lg-6">
+		 								<button class="btn btn-block btn-sosmed">	
+											<img src="public/img/assets/google-icon.png" class="btn-img-sosmed"> <span class="btn-text-sosmed">Google</span>
+		 								</button>
+		 							</div>
 
+		 							<div class="col-lg-12" style="margin-top:55.1px;">
+		 								<p style="font-size:14px;">Atau login dengan email</p>
+		 							</div>
 
-			<!-- RIGHT SIDE TAB -->
-			<div class="col-md-8 offset-md-2 padding-0">
-				<div class="right-side hidden-lg-up hidden-sm-down">
-					<div class="side-content-r">
-						<a class="pull-right register-link2" href="#">Langsung baca buku <i class="fa fa-arrow-right icon-langsung-baca" aria-hidden="true"></i></a>	
-							<div style="margin-top:100px;">
-								<center><img src="<?php echo base_url();?>public/img/logo_purple.png" style="height:100px;">
-								<p class="welcome-message hidden-sm-down" style="font-size:16px;">Selamat datang di Baboo, Buat, Baca, Jual Buku Online</p>
-								<p class="welcome-message hidden-md-up" style="font-size:16px;">Selamat datang di Baboo</p></center>
-								<p class="text-label-login">Lanjutkan dengan</p>
-									<div class="social-login  hidden-lg-up">
-										<div class="row  hidden-sm-down">
-											<div class="col-md-6">
-												<button class="btn btn-facebook btn-block">
-													<img src="<?php echo base_url();?>public/img/assets/combined-shape-copy.svg" class="Combined-Shape-Copy">
-														<span class="btn-teks">Facebook</span>
-												</button>
-											</div>
-											<div class="col-md-6">
-												<button class="btn btn-google btn-block">
-													<img src="<?php echo base_url();?>public/img/google-button.png" class="img-fluid img-button">
-														<span class="btn-teks">Google</span>
-												</button>
-											</div>
-										</div>
-									</div>
-							</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<p class="text-label-login">Atau login dengan email</p>
+		 							<div class="col-lg-12">
+										<form>
+										  <div class="form-group">
+										    <input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+										  </div>
 
-								<!-- FORM -->
-								<form>
-									<div class="form-group">
-										<input oninvalid="this.setCustomValidity('Silahkan masukan email dengan benar, contoh : andi@baboo.id')" type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
-									</div>
-									<div class="form-group">
-										<input type="password" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Password">
-									</div>
-										<p class="text-right register-link" style="margin-top:30px;">Belum punya akun? <a data-toggle="modal" data-target="#exampleModal" href="#" class="register-link2">Daftar disini</a></p>
-										<button class="btn btn-daftar btn-block">Masuk</button>								</form>
-								<!-- END FORM -->
+										  <div class="form-group">
+										    <input type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Password">
+										  </div>
+										  <p class="text-right text-daftar">Belum punya akun ? <a  data-toggle="modal" data-target="#myModal" href="#" class="link-daftar">Daftar disini</a></p>
+										  <button type="submit" class="btn btn-primary pull-right btn-login"><i class="icon-arrow-right"></i></button>
+										</form>
+		 							</div>
 
-							</div>
-						</div>
-					</div>	
-				</div>
-			</div>
-			<!-- END RIGHT SIDE TAB-->
-
-		</div>
+		 						</div>
+		 				</div>
+		 			</div>
+		 		</div>	
+	 	</div>
 	</div>
 </body>
-<!-- END BODY -->
-
 
 <!-- Modal -->
-<div class="modal animated  zoomIn " id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-body">
-      	<div class="konten">
-			<a class="pull-right register-link2"  data-dismiss="modal" href="#">Daftar nanti <i class="fa fa-times icon-langsung-baca" aria-hidden="true"></i></a>
-			<img src="<?php echo base_url();?>public/img/logo_purple.png" style="height:50px; margin-top: 30px;">
-			<p class="welcome-message" style="font-size:16px;">Selamat datang di Baboo</p>
-			<form style="margin-top:30px;">
-				<div class="form-group">
-					<input type="text" class="form-control login-input" placeholder="Nama Lengkap">
-				</div>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true"> &times;</span>
+      	</button>
+      	<div class="row" style="margin-top:30px;">
+      		<div class="col-lg-12 col-xl-12">
+      			<center>
+      				<img src="<?php echo base_url();?>public/img/logo_purple.png" style="height:80px;" class="animated slideInDown">
+		 			<p class="text-img animated slideInDown">Daftar Baboo</p>
+      			</center>
+      		</div>
+      	</div>
+      	<div class="container">
+		 	<div class="col-lg-12 col-xl-12">
+				<form>
+					<div class="form-group">
+						<input type="text" class="form-control login-input" placeholder="Nama Lengkap">
+					</div>
 
-				<div class="form-group">
-					<input oninvalid="this.setCustomValidity('Silahkan masukan email dengan benar, contoh : andi@baboo.id')" type="email" class="form-control login-input" placeholder="Email">
-				</div>
+					<div class="form-group">
+						<input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					</div>
 
-				<div class="form-group">
-					<input type="password" class="form-control login-input" placeholder="Buat Password">
-				</div>
+					<div class="form-group">
+						<input type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Password">
+					</div>
+					<p style="font-size:12px; color:#676767;">Tanggal lahir</p>
 
-				<div class="form-group">
-					<label style="font-size:12px; font-family: 'Proxima Nova', Georgia, sans-serif;">Tanggal Lahir</label>
 					<div class="row">
-						<div class="col-md-4">
-							<select class="form-control login-input">
-								<option>Tanggal</option>
-							</select>
+						<div class="col-lg-4 col-xl-4">
+							<div class="form-group">
+								<select class="form-control login-input">
+									<option>Tanggal</option>
+								</select>
+							</div>
 						</div>
-						<div class="col-md-4">
-							<select class="form-control login-input">
-								<option>Bulan</option>
-							</select>
+						<div class="col-lg-4 col-xl-4">
+							<div class="form-group">
+								<select class="form-control login-input">
+									<option>Bulan</option>
+								</select>
+							</div>
 						</div>
-						<div class="col-md-4">
-							<select class="form-control login-input">
-								<option>Tahun</option>
-							</select>
-						</div>
-					</div>
-				</div>
-				<div class="row" style="margin-top:30px;">
-					<div class="col-md-3">
-						<div class="form-check">
-						  <label class="form-check-label">
-						    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-						    Laki-laki
-						  </label>
+						<div class="col-lg-4 col-xl-4">
+							<div class="form-group">
+								<select class="form-control login-input">
+									<option>Tahun</option>
+								</select>
+							</div>
 						</div>
 					</div>
-					<div class="col-md-6">
-						<div class="form-check">
-						  <label class="form-check-label">
-						    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-						    Perempuan
-						  </label>
+					<div class="row" style="margin-top:10px;">
+						<div class="col-lg-4 col-xl-4">
+							<div class="form-group">
+								<div class="form-check">
+								  <label class="form-check-label">
+								    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked> 
+								    <span class="text-modal">Laki-laki</span>
+								  </label>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-4 col-xl-4">
+							<div class="form-group">
+								<div class="form-check">
+								  <label class="form-check-label">
+								    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+								    <span class="text-modal">Perempuan</span>
+								  </label>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<p style="margin-top:30px;" class="text-center register-link">Dengan mengklik tombol daftar, anda setuju pada <a href="#" class="register-link3"><b>Terms of Service</b></a></p>
-				<button class="btn btn-daftar btn-block">Daftar</button>
-			</form>
-		</div>        
+					<center>
+						<p class="text-daftar" style="text-align:center;">Dengan mengklik tombol daftar, anda setuju pada <br><a  data-toggle="modal" data-target="#myModal" href="#" class="link-daftar"><b>Terms of Service</b></a></p>
+					</center>
+					<button class="btn btn-signup btn-block"><b>Daftar</b></button>
+					</div> 
+				</form>
+		 	</div>
+
+      	</div>
       </div>
     </div>
   </div>
 </div>
 
-
 <!-- Javascript -->
 <script type="text/javascript" src="<?php echo base_url();?>public/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>public/js/tether.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?php echo base_url();?>public/js/bootstrap.min.js"></script>
 </html>
