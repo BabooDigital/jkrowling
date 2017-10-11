62968967d51fe336ccb15d6ab6fba3481f2e7a8d
Aditia Nugraha <aditia@baboo.id>
2017-10-11 11:05:21 +0700

    New Project Web Front-End Dev

---
+++ application/modules/auth/controllers/C_Login.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_Login extends MX_Controller {
+
+	public function index()
+	{
+		if ($this->agent->is_mobile())
+		{
+		        $this->load->view('r_login');
+		}
+		else
+		{
+		        $this->load->view('d_login');
+		}
+	}
+
+}
---
+++ application/modules/r_auth/views/V_Login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Baboo Indonesia</title>
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>bootstrap.css">
+</head>
+
+<body>
+
+</body>
+<!-- JS -->
+<script type="text/javascript" src="<?php echo base_url();?>jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>bootstrap.min.js"></script>
+
+</html>
