e4c025b53c313c7e25e845dcc5e25a070268a516
Aditia Nugraha <aditia@baboo.id>
2017-10-11 19:14:25 +0700

    Add Public

--- public/js/baboo.js
+++ public/js/baboo.js
@@ -1,25 +1,9 @@
 
 // FILTER-GALERI TIMELINE OUT
 $(document).ready(function(){
-    $('.slider6').bxSlider({
-        auto: true,
-        autoHover: true,
-        shrinkItems: true,
-        slideWidth: 500,    
-        minSlides: 1,
-        // moveSlides: 2,
-        // maxSlides: 2,
+    $('.bxslider').bxSlider({
         pager: false,
-        /* Controls must be true */
-        controls: true,
-
-        /* Class selectors from step 1 */
-        nextSelector: '#slider-next',
-        prevSelector: '#slider-prev',
-
-        nextText: '<i class="fa fa-chevron-right fa-2x contslider"></i>',
-        prevText: '<i class="fa fa-chevron-left fa-2x contslider"></i>',
-      });   
+    });
 
     // clicking button with class "category-button"
     $(".btnfilter").click(function(){
@@ -39,7 +23,5 @@ $(document).ready(function(){
             $(".all").filter('.'+filterValue).show("slow");
         }
     });
-
-    $(".stickymenu").stick_in_parent();
 
 });
