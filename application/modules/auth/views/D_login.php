b2c3ce90435456529587bc22c84e8150daf1c084
badar58 <badar@baboo.id>
2017-10-18 16:58:29 +0700

    Add link before login to timeline, copy public asset style login page

--- application/modules/auth/views/D_login.php
+++ application/modules/auth/views/D_login.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 	<title>Login Baboo - Baca buku online</title>
 </head>
@@ -10,60 +9,63 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/animate.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/simple-line-icons.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/fonts/fonts.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/sweetalert2.css">
 <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 <body>
 	<div class="container-fluid">
 		<div class="row no-gutters">
-				<div class="col-lg-8 col-xl-9 nopadding">
+				<div class="col-lg-8 col-md-7 col-xl-9 nopadding">
 		 			<div class="left-side">
 		 				<center>
-		 					<img src="<?php echo base_url();?>public/img/logo_purple.png" class="img-fluid img-left animated slideInDown center-content">
-		 					<p class="text-img animated slideInDown">Selamat datang di Baboo</p>
+		 					<img src="<?php echo base_url();?>public/img/front-image/logo_purple.png" class="img-fluid img-left center-content floating">
+		 					<p class="text-img floating">Baca buku online Indonesia</p>
 		 				</center>
+
 		 			</div>
 		 		</div>
 
-				<div class="col-lg-4 col-xl-3 nopadding">
+				<div class="col-lg-4 col-md-5 col-xl-3 nopadding">
 		 			<div class="right-side">
 		 				<div class="right-side-p">
 		 						<div class="row">
 		 							<div class="col-lg-12">
-		 								<a href="#" class="skip-text">Langsung Baca Buku</a>
+		 								<a href="<?php echo site_url(); ?>timeline/C_timeline/Beranda" class="skip-text" style="font-size:14px;">Langsung Baca Buku</a>
 		 							</div>
 
 		 							<div class="col-lg-12">
-		 								<p class="right-text" style="margin-top:72.2px;">Lanjutkan dengan</p>
+		 								<p class="right-text" style="margin-top:72.2px; font-size:15px; ">Lanjutkan dengan</p>
 		 							</div>
 
-		 							<div class="col-lg-6">
+		 							<div class="col-lg-6 col-md-12 col-xl-6">
 		 								<button class="btn btn-block btn-sosmed">
 											<img src="public/img/assets/fb-icon.svg" class="btn-img-sosmed"> <span class="btn-text-sosmed">Facebook</span>
 										</button>
 		 							</div>
 
-		 							<div class="col-lg-6">
+		 							<div class="col-lg-6 col-md-12 col-xl-6">
 		 								<button class="btn btn-block btn-sosmed">	
-											<img src="public/img/assets/google-icon.png" class="btn-img-sosmed"> <span class="btn-text-sosmed">Google</span>
+											<img src="public/img/assets/google-icon.svg" class="btn-img-sosmed"> <span class="btn-text-sosmed">Google</span>
 		 								</button>
 		 							</div>
 
 		 							<div class="col-lg-12" style="margin-top:55.1px;">
-		 								<p style="font-size:14px;">Atau login dengan email</p>
+		 								<p style="font-size:15px;">Atau login menggunakan email</p>
 		 							</div>
 
 		 							<div class="col-lg-12">
-										<form>
+										<form action="#" method="POST">
 										  <div class="form-group">
-										    <input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+										    <input id="email" type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"  oninvalid="this.setCustomValidity('Email harus menggunakan @')">
 										  </div>
 
 										  <div class="form-group">
-										    <input type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Password">
+										    <input id="password" type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Password">
 										  </div>
 										  <p class="text-right text-daftar">Belum punya akun ? <a  data-toggle="modal" data-target="#myModal" href="#" class="link-daftar">Daftar disini</a></p>
-										  <button type="submit" class="btn btn-primary pull-right btn-login"><i class="icon-arrow-right"></i></button>
+										  <button value="daftar" type="submit" class="btn btn-primary pull-right btn-login"><i class="icon-arrow-right"></i></button>
 										</form>
 		 							</div>
 
@@ -78,83 +80,57 @@
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content" style="width: 105% !important;">
       <div class="modal-body">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true"> &times;</span>
-      	</button>
-      	<div class="row" style="margin-top:30px;">
-      		<div class="col-lg-12 col-xl-12">
-      			<center>
-      				<img src="<?php echo base_url();?>public/img/logo_purple.png" style="height:80px;" class="animated slideInDown">
-		 			<p class="text-img animated slideInDown">Daftar Baboo</p>
-      			</center>
-      		</div>
-      	</div>
       	<div class="container">
+
+      		<div class="col-lg-12 col-xl-12">
+      			<img src="<?php echo base_url();?>public/img/logo_purple.png" style="height:50px; margin-top:36px;">
+      		</div>
+
 		 	<div class="col-lg-12 col-xl-12">
+
+		 		<p class="text-img-modal">Selamat datang di Baboo</p>
 				<form>
 					<div class="form-group">
 						<input type="text" class="form-control login-input" placeholder="Nama Lengkap">
 					</div>
 
 					<div class="form-group">
-						<input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+						<input type="email" class="form-control login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Alamat Email">
 					</div>
 
 					<div class="form-group">
-						<input type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Password">
+						<input type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Kata Sandi">
 					</div>
 					<p style="font-size:12px; color:#676767;">Tanggal lahir</p>
+
+					<div class="form-group">
+						<input  type="text" id="date" data-max-year="2017" data-first-item="name" data-format="DD-MM-YYYY" data-template="D MMM YYYY" data-custom-class="form-control login-input" data-smart-days="true"> 
+					</div>
 
 					<div class="row">
 						<div class="col-lg-4 col-xl-4">
 							<div class="form-group">
-								<select class="form-control login-input">
-									<option>Tanggal</option>
-								</select>
-							</div>
-						</div>
-						<div class="col-lg-4 col-xl-4">
-							<div class="form-group">
-								<select class="form-control login-input">
-									<option>Bulan</option>
-								</select>
-							</div>
-						</div>
-						<div class="col-lg-4 col-xl-4">
-							<div class="form-group">
-								<select class="form-control login-input">
-									<option>Tahun</option>
-								</select>
-							</div>
-						</div>
-					</div>
-					<div class="row" style="margin-top:10px;">
-						<div class="col-lg-4 col-xl-4">
-							<div class="form-group">
 								<div class="form-check">
-								  <label class="form-check-label">
-								    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked> 
-								    <span class="text-modal">Laki-laki</span>
-								  </label>
+								    <input class="badar-radio" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked> 
+								    <span class="text-modal" style="margin-left:15px; font-size:12px;">Laki-laki</span>
 								</div>
 							</div>
 						</div>
 
-						<div class="col-lg-4 col-xl-4">
+						<div class="col-lg-6 col-xl-6">
 							<div class="form-group">
 								<div class="form-check">
-								  <label class="form-check-label">
-								    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-								    <span class="text-modal">Perempuan</span>
-								  </label>
+								    <input class="badar-radio" type="radio" name="exampleRadios" id="exampleRadios1" value="option1"> 
+								    <span class="text-modal" style="margin-left:15px; font-size:12px;">Perempuan</span>
 								</div>
 							</div>
 						</div>
+
 					</div>
 					<center>
-						<p class="text-daftar" style="text-align:center;">Dengan mengklik tombol daftar, anda setuju pada <br><a  data-toggle="modal" data-target="#myModal" href="#" class="link-daftar"><b>Terms of Service</b></a></p>
+						<p class="text-daftar" style="text-align:center;">Dengan mengklik tombol daftar, anda setuju pada <a  data-toggle="modal" data-target="#myModal" href="#" class="link-daftar"><b>Terms of Service</b></a></p>
 					</center>
 					<button class="btn btn-signup btn-block"><b>Daftar</b></button>
 					</div> 
@@ -167,9 +143,32 @@
   </div>
 </div>
 
+
+<!-- FOOTER -->
+<div class="container">
+	<ul style="position: absolute; bottom: 10px; left:20px;">
+  		<li class="footer-link"><a href="#" class="footer-link">Masuk</a></li>
+  		<li class="footer-link m-l-20"><a href="#" class="footer-link">Daftar</a></li>
+  		<li class="footer-link m-l-20"><a href="#" class="footer-link">Tentang Baboo</a></li>
+  		<li class="footer-link m-l-20"><a href="#" class="footer-link">Terms of Use</a></li>
+  		<li class="footer-link m-l-20"><a href="#" class="footer-link">Privacy & Policy</a></li>
+  		<li class="footer-link m-l-20"><a href="#" class="footer-link">Baboo Carrier</a></li>
+  	</ul>
+</div>
+<!-- FOOTER -->
+
 <!-- Javascript -->
 <script type="text/javascript" src="<?php echo base_url();?>public/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>public/js/tether.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?php echo base_url();?>public/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>public/js/moment.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>public/js/combodate.js"></script>
+<script type="text/javascript">
+$(function(){
+    $('#date').combodate('method');
+    firstItem: 'name'	
+});
+</script>
+
 </html>
