6bab73af7fcd23743b50d5321b2215463a093962
badar58 <badar@baboo.id>
2017-10-18 16:58:29 +0700

    Add link before login to timeline, copy public asset style login page

--- application/modules/auth/controllers/C_Login.php
+++ application/modules/auth/controllers/C_Login.php
@@ -5,6 +5,10 @@ class C_Login extends MX_Controller {
 
 	public function index()
 	{
+		if ($this->agent->is_mobile('ipad'))
+		{
+		        $this->load->view('d_login');
+		}
 		if ($this->agent->is_mobile())
 		{
 		        $this->load->view('r_login');
