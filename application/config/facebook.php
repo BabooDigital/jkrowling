761d9b986b99d4bd096b1a33a8eadf783f6d5582
Aditia Nugraha <aditia@baboo.id>
2017-10-30 11:01:18 +0700

    fix bug validasi form login, ubah appid facebook

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -16,11 +16,11 @@
 |  facebook_graph_version        string   Specify Facebook Graph version. Eg v2.6
 |  facebook_auth_on_load         boolean  Set to TRUE to check for valid access token on every page load.
 */
-$config['facebook_app_id']              = '1707577032610102';
-$config['facebook_app_secret']          = 'ecbed98d33ea02eec896b17754b3529f';
+$config['facebook_app_id']              = '124081454991891';
+$config['facebook_app_secret']          = '23b743b45f8fe9a28e5b549b18b8e2be';
 $config['facebook_login_type']          = 'web';
-$config['facebook_login_redirect_url']  = 'auth/c_login/fb_login';
-$config['facebook_logout_redirect_url'] = 'auth/c_login/fb_disconnect';
+$config['facebook_login_redirect_url']  = 'auth/C_Login/fb_login';
+$config['facebook_logout_redirect_url'] = 'auth/C_Login/fb_disconnect';
 $config['facebook_permissions']         = array('email');
 $config['facebook_graph_version']       = 'v2.10';
 $config['facebook_auth_on_load']        = TRUE;
