0b8f86308939bc4bc7cd62e3f87dff64f1ec6af7
rizaldi <rizaldi354313@gmail.com>
2017-11-13 11:42:42 +0700

    bugfixing content timeline

--- application/modules/timeline/views/R_Timeline_in.php
+++ application/modules/timeline/views/R_Timeline_in.php
@@ -23,7 +23,7 @@
 		</div>
 	</div>
 
-	<div class="container babooid">
+	<div class=" babooid" style="overflow-y: hidden;overflow-x: hidden;">
 		<div class="row">
 			<div class="col-md-9">
 				<div class="row">
@@ -42,10 +42,108 @@
 									</div>
 								</div>
 								<div class="row" style="padding: 0px 10px 0px 10px;">
+									<div style="float:left;width:40%;height: auto;">
+					    				<img src="https://spark.adobe.com/images/landing/examples/how-to-book-cover.jpg" style="box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5); height:100%;width: 100%;margin-left: -5px;">
+					    			</div>
+					    			 <div style="float: left;width:60%;height: auto;">
+					                    <div style="padding: 10px;">
+					                    	<b style="font-size: 16px;">Kite Runner - Powerfull Hunting</b>
+							    			<div style="padding-top: 10px;"></div><span style="font-size:12px;border: 1px #7554bd solid;border-radius: 25px;padding: 3px 10px;color: #7554bd;">FIKSI</span>
+							    			<span class="mr-10"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
+							    			<div style="padding-top: 10px;"></div>
+											<div id="content">
+												<p style="font-size:16px; font-family: Roboto;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.</p>
+											</div>
+					                    </div>	
+					                 </div>	
+								</div>
+							</div>
+							<div class="card-footer text-muted" style="font-size: 0.8em;font-weight: bold;">
+								<div class="pull-right">
+									<a href="#"><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg" width="18"></a>
+								</div>
+								<div>
+									<a href="#" class="mr-20"><img src="<?php echo base_url(); ?>public/img/assets/icon_love.svg" class="mr-5" width="27"></a>
+									<a href="#"><img src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" class="mr-10" width="25"></a>
+								</div>
+							</div>
+						</div>
+						<!-- Status -->
+
+						
+						<!-- Status -->
+						<div class="card mb-15" style="padding: 0 00px;box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);height: auto;">
+							<div class="list-group">
+					          <a href="#" class="list-group-item" >
+					                <div style="float: left;">
+					                    <div style="float:left;width:40%;height: auto;">
+
+						    				<img src="https://spark.adobe.com/images/landing/examples/how-to-book-cover.jpg" style="box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5); height:40%;width: 100%;margin-left: -5px;">
+						    			</div>
+						    			 <div style="float: left;width:60%;height: auto;">
+						                    <div style="padding: 10px;">
+						                    	<b style="font-size: 16px;">Kite Runner - Powerfull Hunting</b>
+								    			<div style="padding-top: 10px;"></div><span style="font-size:12px;border: 1px #7554bd solid;border-radius: 25px;padding: 3px 10px;color: #7554bd;">FIKSI</span>
+								    			<span class="mr-10"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
+								    			<div style="padding-top: 10px;"></div>
+								    			<div class="media">
+													<img class="d-flex align-self-start mr-20 rounded-circle" src="<?php echo base_url(); ?>public/img/profile/pp_wanita2.png" width="48" alt="Generic placeholder image">
+													<div class="media-body mt-5">
+														<h5 class="card-title nametitle2">Saraswati</h5>
+														<p class="text-muted" style="margin-top:-10px;"><small><span>Jakarta, Indonesia</span>
+													</div>
+												</div>
+								    			<div style="padding-top: 10px;"></div>
+												<div id="bintang">
+													<span class="fa fa-star checked"></span>
+													<span class="fa fa-star checked"></span>
+													<span class="fa fa-star checked"></span>
+													<span class="fa fa-star"></span>
+													<span class="fa fa-star"></span>
+												</div>
+						                    </div>
+					                </div>
+					                </div>
+					               
+					          </a>
+					          <div class="card-footer text-muted" style="font-size: 0.8em;font-weight: bold;">
+								<div class="pull-right">
+									<a href="#"><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg" width="18"></a>
+								</div>
+								<div>
+									<a href="#" class="mr-20"><img src="<?php echo base_url(); ?>public/img/assets/icon_love.svg" class="mr-5" width="27"></a>
+									<a href="#"><img src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" class="mr-10" width="25"></a>
+								</div>
+							  </div>
+				        </div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class=" babooid" style="overflow-y: hidden;overflow-x: hidden;">
+		<div class="row">
+			<div class="col-md-9">
+				<div class="row">
+					<div class="col-md-6">
+						<!-- Status -->
+						<div class="card mb-15" style="padding: 0 00px;">
+							<div class="card-body p-0 p-20">
+								<div class="row mb-10" style="padding: 0px 10px 0px 10px;">
 									<div class="media">
-										<img src="<?php echo base_url(); ?>public/img/book-cover/cover_hori.png" style="width: 100%;">
+										<img class="d-flex align-self-start mr-20 rounded-circle" src="<?php echo base_url(); ?>public/img/profile/pp_wanita2.png" width="48" alt="Generic placeholder image">
+										<div class="media-body mt-5">
+											<h5 class="card-title nametitle2">Marina Saraswatis</h5>
+											<p class="text-muted" style="margin-top:-10px;"><small><span>Jakarta, Indonesia</span>
+												<span class="ml-10">1 hours ago</span></small></p>
+										</div>
 									</div>
-									<h5 style="padding-top:20px; font-weight: 500;"><b>Story of Diana</b></h5>
+								</div>
+								<div class="row" style="padding: 0px 10px 0px 10px;">
+									<div class="media">
+										<img src="<?php echo base_url(); ?>public/img/book-cover/cover_hori.png" style="width: 100%;height: 130%;">
+									</div>
+									<h5 style="padding-top:50px; font-weight: 500;"><b>Story of Diana</b></h5>
 									<div style="margin-top:10px;">
 									<a href="#" class="mr-10"><span style="font-size:12px;border: 1px #7554bd solid;border-radius: 25px;padding: 3px 10px;color: #7554bd;">FIKSI</span></a> <span class="mr-10"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
 									<p style="font-size:16px; font-family: Roboto; margin-top:20px;	">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.
@@ -281,15 +379,49 @@
 			</div>
 		</div>
 	</div>
-	<div class="container babooid">
+	<div class=" babooid" style="overflow-y: hidden;overflow-x: hidden;">
 		<div class="row">
 			<div class="col-md-9">
 				<div class="row">
 					<div class="col-md-6">
-
-						<label>Buku Pilihan</label>
 						<!-- Status -->
+						<div class="card mb-15" style="padding: 0 00px;">
+							<div class="card-body p-0 p-20">
+								<div class="row mb-10" style="padding: 0px 10px 0px 10px;">
+									<div class="media">
+										<img class="d-flex align-self-start mr-20 rounded-circle" src="<?php echo base_url(); ?>public/img/profile/pp_wanita2.png" width="48" alt="Generic placeholder image">
+										<div class="media-body mt-5">
+											<h5 class="card-title nametitle2">Marina Saraswatis</h5>
+											<p class="text-muted" style="margin-top:-10px;"><small><span>Jakarta, Indonesia</span>
+												<span class="ml-10">1 hours ago</span></small></p>
+										</div>
+									</div>
+								</div>
+								<div class="row" style="padding: 0px 10px 0px 10px;">
+									<div class="media">
+										<img src="<?php echo base_url(); ?>public/img/book-cover/cover_hori.png" style="width: 100%;height: 130%;">
+									</div>
+									<h5 style="padding-top:50px; font-weight: 500;"><b>Story of Diana</b></h5>
+									<div style="margin-top:10px;">
+									<a href="#" class="mr-10"><span style="font-size:12px;border: 1px #7554bd solid;border-radius: 25px;padding: 3px 10px;color: #7554bd;">FIKSI</span></a> <span class="mr-10"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
+									<p style="font-size:16px; font-family: Roboto; margin-top:20px;	">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.
+									</div>
+								</div>
+							</div>
+							<div class="card-footer text-muted" style="font-size: 0.8em;font-weight: bold;">
+								<div class="pull-right">
+									<a href="#"><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg" width="18"></a>
+								</div>
+								<div>
+									<a href="#" class="mr-20"><img src="<?php echo base_url(); ?>public/img/assets/icon_love.svg" class="mr-5" width="27"></a>
+									<a href="#"><img src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" class="mr-10" width="25"></a>
+								</div>
+							</div>
+						</div>
+						<!-- Status -->
+
 						
+						<!-- Status -->
 						<div class="card mb-15" style="padding: 0 00px;box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);height: auto;">
 							<div class="list-group">
 					          <a href="#" class="list-group-item" >
@@ -336,6 +468,9 @@
 				        </div>
 						</div>
 						<!-- Status -->
+
+						
+						<!-- Status -->
 						<div class="card mb-15" style="padding: 0 00px;box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);">
 							<div class="card-body p-0 p-20">
 								<div class="row mb-10" style="padding: 0px 10px 0px 10px;">
@@ -369,8 +504,13 @@
 								</div>
 							</div>
 						</div>
+
+					</div>
+
 				</div>
 			</div>
+		</div>
+	</div>
 </div>
 </div>
 
