c7133a09edae21279a17ca17d7bc360a234e56af
Aditia Nugraha <aditia@baboo.id>
2017-11-23 19:22:03 +0700

    Bug curl in home, change main font family

--- application/modules/timeline/views/D_Timeline_out.php
+++ application/modules/timeline/views/D_Timeline_out.php
@@ -33,8 +33,7 @@
 							Buku Populer
 						</div>
 						<div class="card-body p-0">
-							<ul class="list-group list-group-flush">
-								<?php foreach ($home['data'] as $pop){ ?>	
+							<ul class="list-group list-group-flush">	
 								<li class="list-group-item" style="background-color: transparent;border: none;">
 									<div class="media">
 										<div class="media-left mr-10">
@@ -43,12 +42,11 @@
 										<div class="media-body">
 											<div>
 												<h4 class="media-heading bold mt-10 mb-10"><a href="#">Big Magic: Creative Living Beyon Fear</a></h4>
-												<p style="font-size: 10pt;">by <a href="#"><?php echo $pop['author_name']; ?></a></p>
+												<p style="font-size: 10pt;">by <a href="#">Aditia Nugraha</a></p>
 											</div>
 										</div>
 									</div>
 								</li>
-								<?php } ?>
 							</ul>
 						</div>
 					</div>
@@ -97,72 +95,6 @@
 										<div class="media-body">
 											<h5 class="card-title nametitle3"><a href="<?php echo site_url(); ?>login">Story Of Drama</a></h5>
 											<p class="catbook mb-10"><a class="mr-20" href="#"><span class="btn-no-fill">FIKSI</span></a> <span class="mr-20"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
-											<p class="text-desc-out">Aku tahu ribuan kalimat kau tulis untuk memberi tanda bahwa kamu selalu sabar menungguku, yang seolah-olah kau ingin mengatakan pada dunia bahwa kamu sangat... <a class="readmore" href="#">Lanjut</a></p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="card-footer text-muted" style="font-size: 0.8em;font-weight: bold;">
-								<div class="pull-right">
-									<a href="#"><img class="mr-10" src="<?php echo base_url(); ?>public/img/assets/icon_share.svg" width="18"> Bagikan</a>
-								</div>
-								<div>
-									<a class="mr-30" href="#"><img class="mr-10" src="<?php echo base_url(); ?>public/img/assets/icon_love.svg" width="27"> Suka</a> <a href="#"><img class="mr-10" src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" width="25"> Komentar</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 all komik" style="margin-right: -15px;">
-						<div class="card mb-15" style="padding: 0 10px;">
-							<div class="card-body p-0 p-20">
-								<div class="row mb-30">
-									<div class="media">
-										<img alt="Generic placeholder image" class="d-flex align-self-start mr-20 rounded-circle" src="<?php echo base_url(); ?>public/img/profile/pp_wanita2.png" width="60">
-										<div class="media-body mt-5">
-											<h5 class="card-title nametitle2">Marina Saraswati</h5>
-											<p><small><span>Jakarta, Indonesia</span> <span class="ml-10">1 hours ago</span></small></p>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="media">
-										<img alt="Profile" class="d-flex align-self-start mr-10" src="<?php echo base_url(); ?>public/img/book-cover/book_cover2.png" width="130">
-										<div class="media-body">
-											<h5 class="card-title nametitle3"><a href="<?php echo site_url(); ?>login">Doraemon</a></h5>
-											<p class="catbook mb-10"><a class="mr-20" href="#"><span class="btn-no-fill">KOMIK</span></a> <span class="mr-20"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
-											<p class="text-desc-out">Aku tahu ribuan kalimat kau tulis untuk memberi tanda bahwa kamu selalu sabar menungguku, yang seolah-olah kau ingin mengatakan pada dunia bahwa kamu sangat... <a class="readmore" href="#">Lanjut</a></p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="card-footer text-muted" style="font-size: 0.8em;font-weight: bold;">
-								<div class="pull-right">
-									<a href="#"><img class="mr-10" src="<?php echo base_url(); ?>public/img/assets/icon_share.svg" width="18"> Bagikan</a>
-								</div>
-								<div>
-									<a class="mr-30" href="#"><img class="mr-10" src="<?php echo base_url(); ?>public/img/assets/icon_love.svg" width="27"> Suka</a> <a href="#"><img class="mr-10" src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" width="25"> Komentar</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 all nonfiksi" style="margin-right: -15px;">
-						<div class="card mb-15" style="padding: 0 10px;">
-							<div class="card-body p-0 p-20">
-								<div class="row mb-30">
-									<div class="media">
-										<img alt="Generic placeholder image" class="d-flex align-self-start mr-20 rounded-circle" src="<?php echo base_url(); ?>public/img/profile/pp_wanita2.png" width="60">
-										<div class="media-body mt-5">
-											<h5 class="card-title nametitle2">Marina Saraswati</h5>
-											<p><small><span>Jakarta, Indonesia</span> <span class="ml-10">1 hours ago</span></small></p>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="media">
-										<img alt="Profile" class="d-flex align-self-start mr-10" src="<?php echo base_url(); ?>public/img/book-cover/book_cover3.png" width="130">
-										<div class="media-body">
-											<h5 class="card-title nametitle3"><a href="<?php echo site_url(); ?>login">The Painter's Daughter</a></h5>
-											<p class="catbook mb-10"><a class="mr-20" href="#"><span class="btn-no-fill">NON-FIKSI</span></a> <span class="mr-20"><img src="<?php echo base_url(); ?>public/img/assets/icon_view.svg"> 290</span> <span><img src="<?php echo base_url(); ?>public/img/assets/icon_share.svg"> 12</span></p>
 											<p class="text-desc-out">Aku tahu ribuan kalimat kau tulis untuk memberi tanda bahwa kamu selalu sabar menungguku, yang seolah-olah kau ingin mengatakan pada dunia bahwa kamu sangat... <a class="readmore" href="#">Lanjut</a></p>
 										</div>
 									</div>
