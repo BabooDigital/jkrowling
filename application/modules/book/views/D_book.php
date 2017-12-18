5cb4da3b6743bf44e2bf5f1c186e82d94a2aa648
Aditia Nugraha <aditia@baboo.id>
2017-12-18 15:21:02 +0700

    Like active, Comment, Menu Nav

--- application/modules/book/views/D_book.php
+++ application/modules/book/views/D_book.php
@@ -1,3 +1,46 @@
+<style type="text/css">
+
+/*Right*/
+.modal.right.fade .modal-dialog {
+	right: -435px;
+	-webkit-transition: opacity 0.3s linear, right 0.3s ease-out;
+	-moz-transition: opacity 0.3s linear, right 0.3s ease-out;
+	-o-transition: opacity 0.3s linear, right 0.3s ease-out;
+	transition: opacity 0.3s linear, right 0.3s ease-out;
+}
+
+.modal.right.fade.in .modal-dialog {
+	right: 0;
+}
+.modal-backdrop
+{
+    opacity:0.5 !important;
+}
+
+/* ----- MODAL STYLE ----- */
+.modal-content {
+	border-radius: 0;
+	border: none;
+	height: 100vh;
+}
+
+.modal-header {
+	border-bottom-color: #EEEEEE;
+	background-color: #FAFAFA;
+}
+
+.closes {
+	background: none;
+	font-size: 2rem;
+	line-height: 1;
+	color: #000;
+	text-shadow: 0 1px 0 #fff;
+	opacity: .5;
+	border: none;
+	position: absolute;
+	right: 35px;
+}
+</style>
 <div class="container pt-100 mb-80">
 	<div class="row">
 		<div class="col-md-4 dtlbok">
@@ -84,9 +127,9 @@
 							<p class="mb-30"><a href="#">
 								<img src="<?php echo base_url(); ?>public/img/assets/icon_love.svg" width="40">
 							</a></p>
-							<p><a href="#">
-								<img src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" width="37">
-							</a></p>
+							<p><button type="button" data-toggle="modal" data-target="#myModal2" style="cursor: pointer;background: none;border: none;">
+								<img src="<?php echo base_url(); ?>public/img/assets/icon_comment.svg" width="40">
+							</button></p>
 						</div>
 						<div class="border1px"></div>
 						<div class="pt-20 pb-20 pl-5 pr-5">
@@ -134,6 +177,51 @@
 			<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
 		</div>
 	</nav>
+	<!-- Modal -->
+	<div class="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+
+				<div class="modal-header bg-white">
+					<button type="button" class="closes" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel2">Komentar</h4>
+				</div>
+
+				<div class="modal-body">
+					<div>
+						<div class="media">
+							<img class="d-flex align-self-start mr-20 rounded-circle" width="50" height="50" src="
+							<?php echo base_url('public/img/profile/blank-photo.jpg'); ?>">
+							<div class="media-body">
+								<h5 class="nametitle2 mb-5">Ambo Sayaka</h5>
+								<small><span>Jakarta, Indonesia</span></small>
+							</div>
+						</div>
+						<div class="mt-10">
+							<p class="fs-14px">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+							<a href="#"><b>Balas</b></a>
+						</div>
+						<hr>
+					</div>
+
+					<nav class="navbar navbar-expand-lg navbar-light fixed-bottom box-shadow-navbar">
+						<div class="container pb-10 pt-10">
+							<div class="col-md-9">
+								<!-- <input type="text" class="commentform" name="sendComment" placeholder="Tulis Komentar Kamu"> -->
+								<textarea id="comments" class="commentform" placeholder="Tulis Komentar kamu..."></textarea>
+							</div>
+							<div class="col-md-3">
+								<div>
+									<button>Kirim</button>	
+								</div>
+							</div>
+						</div>
+					</nav>
+				</div>
+
+			</div><!-- modal-content -->
+		</div><!-- modal-dialog -->
+	</div><!-- modal -->
 
 	<!-- Modal -->
 	<script type="text/javascript">
