bab0cdffb00cadd1147dc8dbd621f6b3887ac540
rizaldi <rizaldi354313@gmail.com>
2017-12-19 15:54:27 +0700

    refactor api detail book , infinite scroll detail book

--- application/modules/book/controllers/C_book.php
+++ application/modules/book/controllers/C_book.php
@@ -28,10 +28,94 @@ class C_book extends MX_Controller {
 			'user_id' => $user
 		);
 		// print_r($data_book);
+
 		// START GET CHAPTER
-			
+    	$url = $this->API.'/allChapters/'.$idb[0];
+        $ch = curl_init();
+        $options = array(
+        	  CURLOPT_URL			 => $url,
+        	  CURLOPT_RETURNTRANSFER => true,
+	          CURLOPT_CUSTOMREQUEST  =>"GET",    // Atur type request
+	          CURLOPT_POST           =>false,    // Atur menjadi GET
+	          CURLOPT_FOLLOWLOCATION => false,    // Follow redirect aktif
+	          CURLOPT_SSL_VERIFYPEER => 0,
+	          CURLOPT_HEADER         => 1,
+	          CURLOPT_HTTPHEADER	 => array('baboo-auth-key : '.$auth)
+
+        );
+        curl_setopt_array($ch, $options);
+        $content = curl_exec($ch);
+        curl_close($ch);
+        $headers=array();
+        
+        $data_before_chapter=explode("\n",$content);
+        $headers['status']=$data_before_chapter[0];
+
+		array_shift($data_before_chapter);
+
+		foreach($data_before_chapter as $part){
+		    $middle=explode(":",$part);
+		    $headers[trim($middle[0])] = trim($middle[1]);
+		}
+
+        $data_before_chapter['chapter'] = json_decode($data_before_chapter[14], true);
+        $auth = $headers['BABOO-AUTH-KEY'];
+        if (isset($data_before_chapter['chapter']['code']) && $data_before_chapter['chapter']['code'] == '200')
+        {
+            $status = $data_before_chapter['chapter']['code'];
+           	$this->session->set_userdata('authKey', $auth);
+        }
+        else
+        {
+            $status = $data_before_chapter['chapter']['code'];
+        }
+
 		// END GET CHAPTER
+		
 		$ch = curl_init();
+    	$url = $this->API.'/detailBook/';
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_book);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('baboo-auth-key : '.$auth));
+        $content = curl_exec($ch);
+        $headers=array();
+        
+        $data=explode("\n",$content);
+        $headers['status']=$data[0];
+
+		array_shift($data);
+
+		foreach($data as $part){
+		    $middle=explode(":",$part);
+		    $headers[trim($middle[0])] = trim($middle[1]);
+		}
+
+        $data['detail_book'] = json_decode($data[16], true);
+        $auth = $headers['BABOO-AUTH-KEY'];
+        
+       	$this->session->set_userdata('authKey', $auth);
+
+
+        if (!$this->input->get("chapter")) {
+	        
+        }else{
+
+	        $chapter_id = $data_before_chapter['chapter']['data'][$this->input->get("chapter")]['chapter_id'];
+
+        	$url = $this->API.'/detailBook/';
+        	$data_book = array(
+				'book_id' => $idb[0],
+				'user_id' => $user,
+				'chapter' => $chapter_id
+			);
+
+	        $ch = curl_init();
         	$url = $this->API.'/detailBook/';
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -44,10 +128,9 @@ class C_book extends MX_Controller {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array('baboo-auth-key : '.$auth));
 	        $content = curl_exec($ch);
 	        $headers=array();
-	        
 	        $data=explode("\n",$content);
 	        $headers['status']=$data[0];
-
+	        // print_r($data);
 			array_shift($data);
 
 			foreach($data as $part){
@@ -55,90 +138,7 @@ class C_book extends MX_Controller {
 			    $headers[trim($middle[0])] = trim($middle[1]);
 			}
 
-	        $data['detail_book'] = json_decode($data[14], true);
-	        $auth = $headers['BABOO-AUTH-KEY'];
-	        
-           	$this->session->set_userdata('authKey', $auth);
-
-           	print_r($content);
-
-        if (!$this->input->get("chapter")) {
-	        
-        }else{
-        	$url = $this->API.'/allChapters/'.$idb[0];
-	        $ch = curl_init();
-	        $options = array(
-	        	  CURLOPT_URL			 => $url,
-	        	  CURLOPT_RETURNTRANSFER => true,
-		          CURLOPT_CUSTOMREQUEST  =>"GET",    // Atur type request
-		          CURLOPT_POST           =>false,    // Atur menjadi GET
-		          CURLOPT_FOLLOWLOCATION => false,    // Follow redirect aktif
-		          CURLOPT_SSL_VERIFYPEER => 0,
-		          CURLOPT_HEADER         => 1,
-		          CURLOPT_HTTPHEADER	 => array('baboo-auth-key : '.$auth)
-
-	        );
-	        curl_setopt_array($ch, $options);
-	        $content = curl_exec($ch);
-	        curl_close($ch);
-	        $headers=array();
-	        
-	        $data_before_chapter=explode("\n",$content);
-	        $headers['status']=$data_before_chapter[0];
-
-			array_shift($data_before_chapter);
-
-			foreach($data_before_chapter as $part){
-			    $middle=explode(":",$part);
-			    $headers[trim($middle[0])] = trim($middle[1]);
-			}
-
-	        $data_before_chapter['chapter'] = json_decode($data_before_chapter[14], true);
-	        $auth = $headers['BABOO-AUTH-KEY'];
-	        if (isset($data_before_chapter['chapter']['code']) && $data_before_chapter['chapter']['code'] == '200')
-	        {
-	            $status = $data_before_chapter['chapter']['code'];
-	           	$this->session->set_userdata('authKey', $auth);
-	        }
-	        else
-	        {
-	            $status = $data_before_chapter['chapter']['code'];
-	        }
-
-
-			$id = '/'.$this->input->get("chapter");
-
-	        $chapter_id = '/'.$data_before_chapter['chapter']['data'][$this->input->get("chapter")]['chapter_id'];
-
-        	$url = $this->API.'/detailBook/'.$idb[0].$chapter_id;
-	        $ch = curl_init();
-	        $options = array(
-	        	  CURLOPT_URL			 => $url,
-	        	  CURLOPT_RETURNTRANSFER => true,
-		          CURLOPT_CUSTOMREQUEST  =>"POST",    // Atur type request
-		          CURLOPT_POST           =>false,    // Atur menjadi GET
-		          CURLOPT_FOLLOWLOCATION => false,    // Follow redirect aktif
-		          CURLOPT_SSL_VERIFYPEER => 0,
-		          CURLOPT_HEADER         => 1,
-		          CURLOPT_HTTPHEADER	 => array('baboo-auth-key : '.$auth)
-
-	        );
-	        curl_setopt_array($ch, $options);
-	        $content = curl_exec($ch);
-	        curl_close($ch);
-	        $headers=array();
-	        
-	        $data=explode("\n",$content);
-	        $headers['status']=$data[0];
-
-			array_shift($data);
-
-			foreach($data as $part){
-			    $middle=explode(":",$part);
-			    $headers[trim($middle[0])] = trim($middle[1]);
-			}
-
-	        $data['detail_book'] = json_decode($data[14], true);
+	        $data['detail_book'] = json_decode($data[16], true);
 	        $auth = $headers['BABOO-AUTH-KEY'];
 	        if (isset($data['detail_book']['code']) && $data['detail_book']['code'] == '200')
 	        {
@@ -152,7 +152,8 @@ class C_book extends MX_Controller {
         }
         
 		$data['judul'] = $data['detail_book']['data']['book_info']['title_book']." - Baboo";
-		$data['detailBook'] = json_decode($data[14], true);
+
+		$data['detailBook'] = json_decode($data[16], true);
 		$data['detailChapter'] = count($data_before_chapter['chapter']) - 1;
 		$data['js'][] = "public/js/jquery.min.js";
 		$data['js'][] = "public/js/umd/popper.min.js";
@@ -166,17 +167,15 @@ class C_book extends MX_Controller {
 		}else{
 			if ($this->input->get("chapter")) {
 				if ($data_before_chapter['chapter']['data'][$this->input->get("chapter")] == null || $data_before_chapter['chapter']['data'][$this->input->get("chapter")] == '') {
-		        	print_r("kosong chapter");
+		        	// print_r("kosong chapter");
 		        }else{
 					$result = $this->load->view('data/D_book', $data);
 		        }
 			}else{	
 				$this->load->view('timeline/include/head', $data);
 				$this->load->view('D_book', $data);
-				// print_r($content);
 			}
 		}
-		// $this->load->view('timeline/include/foot');
 	}
 
 	public function chapter()
@@ -300,10 +299,10 @@ class C_book extends MX_Controller {
 		$data['js'][] = "public/js/bootstrap.min.js";
 		$data['js'][] = "public/js/custom/reading_mode.js";
 		$this->load->view('D_readingmode', $data);
-		// if (!empty($this->input->get("page"))) {
-		// 	$result = $this->load->view('data/D_readingmode', $datas);
-		// }else{
-		// 	$this->load->view('D_readingmode', $datas);
-		// }
+		if (!empty($this->input->get("page"))) {
+			$result = $this->load->view('data/D_readingmode', $datas);
+		}else{
+			$this->load->view('D_readingmode', $datas);
+		}
 	}
 }
--- application/modules/book/views/data/D_book.php
+++ application/modules/book/views/data/D_book.php
@@ -1,4 +1,5 @@
-<h2 id="">Chapter : <?php print_r($detail_book['data']['chapter']['chapter_title']); ?></h2>
+<br>
+<h2 id="">Chapter <?php echo count($detail_book['data']['chapter']); ?> : <?php print_r($detail_book['data']['chapter']['chapter_title']); ?></h2>
 <?php 
 foreach ($detail_book['data']['chapter']['paragraphs'] as $book) {
 	$data .= $book['paragraph_text'];
