5b90f5e7e2339f03a4dd7c9b739875642c278f4a
rizaldi <rizaldi354313@gmail.com>
2018-01-04 13:34:28 +0700

    redesign createbook

--- application/modules/book/views/D_createbook.php
+++ application/modules/book/views/D_createbook.php
@@ -11,36 +11,7 @@
 	<?php endif ?>
 </head>
 <script type="text/javascript">
-		function tampilkanPreview(gambar, idpreview) {
-		// membuat objek gambar
-		var gb = gambar.files;
-
-		// loop untuk merender gambar
-		for (var i = 0; i < gb.length; i++) {
-			// bikin variabel
-			var gbPreview = gb[i];
-			var imageType = /image.*/;
-			var preview = document.getElementById(idpreview);
-			var reader = new FileReader();
-
-			if (gbPreview.type.match(imageType)) {
-				// jika tipe data sesuai
-				preview.file = gbPreview;
-				reader.onload = (function(element) {
-					return function(e) {
-						element.src = e.target.result;
-					};
-				})(preview);
-
-				// membaca data URL gambar
-				reader.readAsDataURL(gbPreview);
-			} else {
-				// jika tipe data tidak sesuai
-				alert("Type file tidak sesuai. Khusus image.");
-			}
-
-		}
-	}
+		
 </script>
 <body>
 	<form action="<?php echo site_url(); ?>my_book/create_book/publish" id="form_book" method="POST" enctype="multipart/form-data">
