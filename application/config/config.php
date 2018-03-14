a388e996a0ebf3b5e065b2dd54cb09620b52eda5
Aditia Nugraha <aditia@baboo.id>
2018-03-15 01:24:41 +0700

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -30,9 +30,9 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root=(isset($_SERVER['HTTPS']) ? "https://" : "").$_SERVER['HTTP_HOST'];
-$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $root;
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['composer_autoload'] = TRUE;
 /*
 |--------------------------------------------------------------------------
