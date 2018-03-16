ed0f5e7de100b5f9723130e71232784f8d2c623c
Aditia Nugraha <aditia@baboo.id>
2018-03-16 08:27:54 +0700

    fix bugs other profile

---
+++ public/js/custom/mobile/r_other_profile.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+
+  $(document).on('click', '.like', function() {
+    var aww = $(this);
+    var formData = new FormData();
+
+    aww.children('.loveicon').attr("src", "../public/img/assets/love_active.svg");
+    formData.append("user_id", $("#iaiduui").val());
+    formData.append("book_id", aww.attr("data-id"));
+    $.ajax({
+      url: base_url + 'like',
+      type: 'POST',
+      dataType: 'JSON',
+      cache: false,
+      contentType: false,
+      processData: false,
+      data: formData,
+    })
+    .done(function(data) {
+        // $('.loader').hide();
+        if (data.code == null || data.code == 403 || data.code == 404) {
+          aww.children('.loveicon').attr("src", "../public/img/assets/icon_love.svg");
+        } else {
+          aww.removeClass('like');
+          aww.addClass('unlike');
+          aww.children('.loveicon').attr("src", "../public/img/assets/love_active.svg");
+        }
+      })
+    .fail(function() {
+      console.log("Failure");
+    })
+    .always(function() {});
+
+  });
+
+  // UNLIKE BUTTON
+  $(document).on('click', '.unlike', function() {
+
+    var aww = $(this);
+    var formData = new FormData();
+
+    aww.children('.loveicon').attr("src", "../public/img/assets/icon_love.svg");
+    formData.append("user_id", $("#iaiduui").val());
+    formData.append("book_id", aww.attr("data-id"));
+    $.ajax({
+      url: base_url + 'like',
+      type: 'POST',
+      dataType: 'JSON',
+      cache: false,
+      contentType: false,
+      processData: false,
+      data: formData,
+        // beforeSend: function() {
+        // }
+      })
+    .done(function(data) {
+        // $('.loader').hide();
+        if (data.code == null || data.code == 403 || data.code == 404) {
+          aww.children('.loveicon').attr("src", "../public/img/assets/love_active.svg");
+        } else {
+          aww.removeClass('unlike');
+          aww.addClass('like');
+          aww.children('.txtlike').text('Suka');
+          aww.children('.loveicon').attr("src", "../public/img/assets/icon_love.svg");
+        }
+      })
+    .fail(function() {
+      console.log("Failure");
+    })
+    .always(function() {});
+
+  });
+
+  $(document).on('click', '.share-fb', function() {
+
+    var aww = $(this);
+    var formData = new FormData();
+    var blink = aww.parents(".card").find(".dbooktitle").text();
+    var desc = aww.parents(".card").find(".textp").attr('data-text')+'.. - Baca buku lebih lengkap disini.. | Baboo - Beyond Book & Creativity';
+    var img = aww.parents(".card").find('.cover_image').attr('src');
+    var auname = aww.parents(".card").find('.author_name').text();
+    var segment = aww.parents(".card").find('.segment').attr('data-href');
+    console.log(blink);
+    console.log(desc);
+    console.log(img);
+    console.log(auname);
+    console.log(segment);
+    FB.ui({
+      method     : 'share_open_graph',
+      action_type: 'og.shares',
+      action_properties: JSON.stringify({
+        object: {
+          'og:url': base_url + 'book/' + convertToSlug(segment) + '/preview',
+          'og:title': blink + ' ~ By : ' + auname + ' | Baboo - Beyond Book & Creativity',
+          'og:description': desc,
+          'og:image': img
+        }
+      })
+    },
+      // callback
+      function(response) {
+        if (response && !response.error_message) {
+
+          formData.append("user_id", $("#iaiduui").val());
+          formData.append("book_id", aww.attr('data-share'));
+
+          $.ajax({
+            url: base_url + 'shares',
+            type: 'POST',
+            dataType: 'JSON',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: formData,
+              // beforeSend: function() {
+              // }
+            })
+          .done(function(data) {
+              // $('.loader').hide();
+              // console.log("ke share");
+            })
+          .fail(function() {
+            console.log("Failure");
+          })
+          .always(function() {
+
+          });
+        } else {
+          console.log("Batal Share");
+        }
+      });
+  });
+
+  function convertToSlug(Text) {
+    return Text
+      .toLowerCase()
+      .replace(/[^\w ]+/g, '')
+      .replace(/ +/g, '-');
+  }
+});
--- public/js/custom/mobile/r_profile_page.js
+++ public/js/custom/mobile/r_profile_page.js
@@ -2,6 +2,138 @@ $(document).ready(function() {
 
 	var id = $('#iaiduui').val();
 
+	// LIKE BUTTON
+  $(document).on('click', '.like', function() {
+    var aww = $(this);
+    var formData = new FormData();
+
+    aww.children('.loveicon').attr("src", "public/img/assets/love_active.svg");
+    formData.append("user_id", $("#iaiduui").val());
+    formData.append("book_id", aww.attr("data-id"));
+    $.ajax({
+      url: base_url + 'like',
+      type: 'POST',
+      dataType: 'JSON',
+      cache: false,
+      contentType: false,
+      processData: false,
+      data: formData,
+    })
+    .done(function(data) {
+        // $('.loader').hide();
+        if (data.code == null || data.code == 403 || data.code == 404) {
+          aww.children('.loveicon').attr("src", "public/img/assets/icon_love.svg");
+        } else {
+          aww.removeClass('like');
+          aww.addClass('unlike');
+          aww.children('.loveicon').attr("src", "public/img/assets/love_active.svg");
+        }
+      })
+    .fail(function() {
+      console.log("Failure");
+    })
+    .always(function() {});
+
+  });
+
+  // UNLIKE BUTTON
+  $(document).on('click', '.unlike', function() {
+
+    var aww = $(this);
+    var formData = new FormData();
+    var txt = + aww.parents(".card").find(".like_countys").text() - 1;
+
+    aww.children('.loveicon').attr("src", "public/img/assets/icon_love.svg");
+    formData.append("user_id", $("#iaiduui").val());
+    formData.append("book_id", aww.attr("data-id"));
+    $.ajax({
+      url: base_url + 'like',
+      type: 'POST',
+      dataType: 'JSON',
+      cache: false,
+      contentType: false,
+      processData: false,
+      data: formData,
+        // beforeSend: function() {
+        // }
+      })
+    .done(function(data) {
+        // $('.loader').hide();
+        if (data.code == null || data.code == 403 || data.code == 404) {
+          aww.children('.loveicon').attr("src", "public/img/assets/love_active.svg");
+        } else {
+          aww.removeClass('unlike');
+          aww.addClass('like');
+          aww.parents(".card").find(".like_countys").text(txt);
+          aww.children('.txtlike').text('Suka');
+          aww.children('.loveicon').attr("src", "public/img/assets/icon_love.svg");
+        }
+      })
+    .fail(function() {
+      console.log("Failure");
+    })
+    .always(function() {});
+
+  });
+
+	$(document).on('click', '.share-fb', function() {
+
+    var aww = $(this);
+    var formData = new FormData();
+    var bid = $('#iaidubi').val();
+    var blink = aww.parents(".card").find(".dbooktitle").text();
+    var txt = + aww.parents(".card").find(".share_countys").text() + 1;
+    var desc = aww.parents(".card").find(".textp").attr('data-text')+'.. - Baca buku lebih lengkap disini.. | Baboo - Beyond Book & Creativity';
+    var img = aww.parents(".card").find('.cover_image').attr('src');
+    var auname = aww.parents(".card").find('.author_name').text();
+    var segment = aww.parents(".card").find('.segment').attr('data-href');
+
+    FB.ui({
+      method     : 'share_open_graph',
+      action_type: 'og.shares',
+      action_properties: JSON.stringify({
+        object: {
+          'og:url': base_url + 'book/' + convertToSlug(segment) + '/preview',
+          'og:title': blink + ' ~ By : ' + auname + ' | Baboo - Beyond Book & Creativity',
+          'og:description': desc,
+          'og:image': img
+        }
+      })
+    },
+      // callback
+      function(response) {
+        if (response && !response.error_message) {
+
+          formData.append("user_id", $("#iaiduui").val());
+          formData.append("book_id", aww.attr('data-share'));
+
+          $.ajax({
+            url: base_url + 'shares',
+            type: 'POST',
+            dataType: 'JSON',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: formData,
+              // beforeSend: function() {
+              // }
+            })
+          .done(function(data) {
+              // $('.loader').hide();
+              // console.log("ke share");
+            })
+          .fail(function() {
+            console.log("Failure");
+          })
+          .always(function() {
+
+          });
+        } else {
+          console.log("Batal Share");
+        }
+      });
+  });
+
 	function convertToSlug(Text) {
 	  return Text
 	    .toLowerCase()
@@ -34,7 +166,16 @@ $(document).ready(function() {
 		        } else if (item.cover_url == "") {
 		          cover = 'public/img/profile/blank-photo.jpg';
 		        }
-				datas += "<div class='card mb-15 p-0'> <div class='card-body p-0 pl-30 pr-30 pt-15'> <div class='row mb-10 pl-15 pr-15'> <div class='media'> <img class='d-flex align-self-start mr-20 rounded-circle' src='"+ item.author_avatar +"' width='50' height='50' alt='"+ item.author_name +"'> <div class='media-body mt-5'> <h5 class='card-title nametitle2'><a href='javascript:void(0);'>"+ item.author_name +"</a></h5> <p class='text-muted' style='margin-top:-10px;'><small><span>Jakarta, Indonesia</span> <span class='ml-10'>"+ item.publish_date +"</span></small></p> </div> </div> </div> <div class='media'> <a href='book/"+ item.book_id+"-"+convertToSlug(item.title_book) +"'><img alt='"+item.title_book+"' src='"+cover+"' class='w-100 imgcover'></a> </div> <a href='book/"+ item.book_id+"-"+convertToSlug(item.title_book) +"'><h5 class='pt-20 w-100' style='font-weight: 700;'><b>"+item.title_book+"</b></h5></a> <div class='w-100'> <span class='mr-8' style='font-size: 12px;'>"+item.category+" &#8226;</span> <span class='text-muted' style='font-size: 11px;'>Dibaca "+ item.view_count +" kali</span> <p class='mt-10'>"+ desc.substr(0, 100)+'...' +"</p> </div> </div> <div class='bg-white card-footer text-muted pr-30 pl-30' style='font-size: 0.8em;font-weight: bold;'> <div class='pull-right'> <a href='#'><img src='public/img/assets/icon_share.svg' width='23'></a> </div> <div> <a data-id='"+ item.book_id+"' href='javascript:void(0);' id='loveboo"+ item.book_id+"'><img src='public/img/assets/icon_love.svg' class='mr-20 loveicon' width='27'></a> <a href='javascript:void(0);'><img src='public/img/assets/icon_comment.svg' class='mr-10' width='25'></a> </div> </div> </div>"; 
+		        var like;
+		        var likeimg;
+		        if (item.is_like == true) {
+		        	like = 'unlike';
+		        	likeimg = 'public/img/assets/icon_love.svg';
+		        }else {
+		        	like = 'like';
+		        	likeimg = 'public/img/assets/love_active.svg';
+		        }
+				datas += "<div class='card mb-15 p-0'> <div class='card-body p-0 pl-30 pr-30 pt-15'> <div class='row mb-10 pl-15 pr-15'> <div class='media'> <img class='d-flex align-self-start mr-20 rounded-circle' src='"+ item.author_avatar +"' width='50' height='50' alt='"+ item.author_name +"'> <div class='media-body mt-5'> <h5 class='card-title nametitle2 author_name'><a href='javascript:void(0);'>"+ item.author_name +"</a></h5> <p class='text-muted' style='margin-top:-10px;'><small><span>Jakarta, Indonesia</span> <span class='ml-10'>"+ item.publish_date +"</span></small></p> </div> </div> </div> <div class='media'> <a href='book/"+ item.book_id+"-"+convertToSlug(item.title_book) +"'><img alt='"+item.title_book+"' src='"+cover+"' class='w-100 imgcover cover_image'></a> </div> <a href='book/"+ item.book_id+"-"+convertToSlug(item.title_book) +"' class='segment' data-href='"+ item.book_id+"-"+convertToSlug(item.title_book) +"'><h5 class='pt-20 w-100' style='font-weight: 700;'><b class='dbooktitle'>"+item.title_book+"</b></h5></a> <div class='w-100'> <span class='mr-8' style='font-size: 12px;'>"+item.category+" &#8226;</span> <span class='text-muted' style='font-size: 11px;'>Dibaca "+ item.view_count +" kali</span> <p class='mt-10 textp' data-text='"+ desc.substr(0, 100)+'...' +"'>"+ desc.substr(0, 100)+'...' +"</p> </div> </div> <div class='bg-white card-footer text-muted pr-30 pl-30' style='font-size: 0.8em;font-weight: bold;'> <div class='pull-right'> <div class='dropdown'> <button class='share-btn dropbtn' type='button' id='dropShare' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> <img src='public/img/assets/icon_share.svg' width='23'> </button> <div class='dropdown-menu' aria-labelledby='dropShare'> <a class='dropdown-item share-fb' href='javascript:void(0);' data-share=''><img src='public/img/assets/fb-icon.svg' width='20'> Facebook</a> </div> </div> </div> <div> <a data-id='"+ item.book_id+"' href='javascript:void(0);' class='"+like+"' id='loveboo"+ item.book_id+"'><img src='"+likeimg+"' class='mr-20 loveicon' width='27'></a> <a href='book/"+ item.book_id+"-"+convertToSlug(item.title_book) +"#comments'><img src='public/img/assets/icon_comment.svg' class='mr-10' width='25'></a> </div> </div> </div>"; 
 			});
 		}
 			$(".loader").hide();
