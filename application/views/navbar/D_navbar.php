8548f0ec0c91332aba2cb0f27d455f7cd1f03823
Aditia Nugraha <aditia@baboo.id>
2018-03-21 17:13:09 +0700

    del style unfunction

--- application/views/navbar/D_navbar.php
+++ application/views/navbar/D_navbar.php
@@ -39,7 +39,7 @@
 						<div id="noti_Counter"></div>
 						<a id="noti_Button" class="nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#"><img src="<?php echo site_url(); ?>public/img/icon-tab/notif_icon.svg" width="25"><p class="fs-12px"><b>Notification</b></p></a>
 						<!-- <div id="noti_Counter2" style="display: none;">7</div> -->
-						<div id="notifications" style="margin-left: 50%;overflow-y: scroll;">
+						<div id="notifications">
 							<h5>Notifications</h5>
 							<div class="loader mt-10 mb-10" id="loader_notif" style="margin-left: auto;margin-right: auto;"></div>
 							<div class="list-group" id="notification">
