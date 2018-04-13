afb555d743223c87619760ef25e94b0019d974af
Aditia Nugraha <aditia@baboo.id>
2018-04-13 16:47:42 +0700

    after form full fill redirect action

--- application/modules/pin/views/third_otp.php
+++ application/modules/pin/views/third_otp.php
@@ -28,16 +28,16 @@
 					<div class="row">
 						<form id="formOTP" style="display: flex;">
 							<div class="col-3 pr-5 pl-5">
-								<input name="firstdigit" class="form-control digit text-center first" type="number" onchange="checkAndSubmit()" required id="firstdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="0">
+								<input name="firstdigit" class="form-control digit text-center first" type="number" required id="firstdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="0">
 							</div>
 							<div class="col-3 pr-5 pl-5">
-								<input name="secondtdigit" class="form-control digit text-center" type="number" onchange="checkAndSubmit()" required id="secondtdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="1">
+								<input name="secondtdigit" class="form-control digit text-center" type="number" required id="secondtdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="1">
 							</div>
 							<div class="col-3 pr-5 pl-5">
-								<input name="thirddigit" class="form-control digit text-center" type="number" onchange="checkAndSubmit()" required id="thirddigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1"  tabindex="2" >
+								<input name="thirddigit" class="form-control digit text-center" type="number" required id="thirddigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1"  tabindex="2" >
 							</div>
 							<div class="col-3 pr-5 pl-5">
-								<input name="fourthdigit" class="form-control digit text-center" type="number" onchange="checkAndSubmit()" required id="fourthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="3">
+								<input name="fourthdigit" class="form-control digit text-center" type="number" required id="fourthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="3">
 							</div>
 						</form>
 					</div>
@@ -64,6 +64,11 @@
 			$("#firstdigit").focus();
 
 			$(":input[type='number']").keyup(function(event){
+				var a = $( "#firstdigit" );
+				var b = $( "#secondtdigit" );
+				var c = $( "#thirddigit" );
+				var d = $( "#fourthdigit" );
+
 				if ($(this).next('[type="number"]').length > 0){
 					$(this).next('[type="number"]')[0].focus();
 				}else{
@@ -71,21 +76,13 @@
 						$(this).parent().next().find('[type="number"]')[0].focus();
 					}
 				}
-			});
-		});
-
-		function checkAndSubmit()
-		{
-			var a = $( "#firstdigit" );
-			var b = $( "#secondtdigit" );
-			var c = $( "#thirddigit" );
-			var d = $( "#fourthdigit" );
-			if (a.val().length > 0 && b.val().length > 0 && c.val().length > 0 && d.val().length > 0)
-			{
+				if (a.val().length > 0 && b.val().length > 0 && c.val().length > 0 && d.val().length > 0)
+				{
 			    // $('#formOTP').submit();
 			    window.location = base_url+'pin_dompet/fourth';
 			}
-		}
+		});
+		});
 	</script>
 </body>
 </html>
