4a5fb6173dd780e7245b8b9c03d94b094c2d0429
Aditia Nugraha <aditia@baboo.id>
2018-04-13 16:47:42 +0700

    after form full fill redirect action

--- application/modules/pin/views/fifth_confirmpin.php
+++ application/modules/pin/views/fifth_confirmpin.php
@@ -22,22 +22,22 @@
 				<div class="col-10">
 					<div class="row">
 						<div class="col-2 pr-5 pl-5">
-							<input name="firstdigit" class="form-control text-center pininput" type="number" onchange="checkAndSubmit()" required id="firstdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="0">
+							<input name="firstdigit" class="form-control text-center pininput" type="number" required id="firstdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="0">
 						</div>
 						<div class="col-2 pr-5 pl-5">
-							<input name="secondtdigit" class="form-control text-center pininput" type="number" onchange="checkAndSubmit()" required id="secondtdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="1">
+							<input name="secondtdigit" class="form-control text-center pininput" type="number" required id="secondtdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="1">
 						</div>
 						<div class="col-2 pr-5 pl-5">
-							<input name="thirddigit" class="form-control text-center pininput" type="number" onchange="checkAndSubmit()" required id="thirddigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1"  tabindex="2" >
+							<input name="thirddigit" class="form-control text-center pininput" type="number" required id="thirddigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1"  tabindex="2" >
 						</div>
 						<div class="col-2 pr-5 pl-5">
-							<input name="fourthdigit" class="form-control text-center pininput" type="number" onchange="checkAndSubmit()" required id="fourthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="3">
+							<input name="fourthdigit" class="form-control text-center pininput" type="number" required id="fourthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="3">
 						</div>
 						<div class="col-2 pr-5 pl-5">
-							<input name="fifthdigit" class="form-control text-center pininput" type="number" onchange="checkAndSubmit()" required id="fifthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="4">
+							<input name="fifthdigit" class="form-control text-center pininput" type="number" required id="fifthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="4">
 						</div>
 						<div class="col-2 pr-5 pl-5">
-							<input name="sixthdigit" class="form-control text-center pininput" type="number" onchange="checkAndSubmit()" required id="sixthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="5">
+							<input name="sixthdigit" class="form-control text-center pininput" type="number" required id="sixthdigit" size="1" minlength="1" onKeyPress="if(this.value.length==1) return false;" maxlength="1" tabindex="5">
 						</div>
 					</div>
 				</div>
@@ -55,6 +55,13 @@
 			$("#firstdigit").focus();
 
 			$(":input[type='number']").keyup(function(event){
+				var a = $( "#firstdigit" );
+				var b = $( "#secondtdigit" );
+				var c = $( "#thirddigit" );
+				var d = $( "#fourthdigit" );
+				var e = $( "#fifthdigit" );
+				var f = $( "#sixthdigit" );
+
 				if ($(this).next('[type="number"]').length > 0){
 					$(this).next('[type="number"]')[0].focus();
 				}else{
@@ -63,24 +70,14 @@
 
 					}
 				}
-			});
 
-		});
-
-		function checkAndSubmit()
-		{
-			var a = $( "#firstdigit" );
-			var b = $( "#secondtdigit" );
-			var c = $( "#thirddigit" );
-			var d = $( "#fourthdigit" );
-			var e = $( "#fifthdigit" );
-			var f = $( "#sixthdigit" );
-			if (a.val().length > 0 && b.val().length > 0 && c.val().length > 0 && d.val().length > 0 && e.val().length > 0 && f.val().length > 0)
-			{
+				if (a.val().length > 0 && b.val().length > 0 && c.val().length > 0 && d.val().length > 0 && e.val().length > 0 && f.val().length > 0)
+				{
 			    // $('#formPin').submit();
 			    window.location = base_url+'pin_dompet/sixth';
 			}
-		}
+		});
+		});
 	</script>
 </body>
 </html>
