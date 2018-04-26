f383d3f20a7b6b7b3455258caeda2f151f7cc6bf
Aditia Nugraha <aditia@baboo.id>
2018-04-26 11:23:10 +0700

    change appid & secret only staging login socmed

--- application/config/google.php
+++ application/config/google.php
@@ -14,18 +14,8 @@
 
 	$CI =& get_instance();
 	$base = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-	$clientid = '625200931795-v9j07677ch7drvplu5ohpph4u48b4277.apps.googleusercontent.com';
-	$clientsecret = '9a-ThTIiKvuELANSjq-HFn4g';
-	if (strpos($base, Curl_Request::staging) !== false) {
-		$clientid = '347878416490-7era9p9mn379qdod4vbd61s217ubk2co.apps.googleusercontent.com';
-		$clientsecret = 'TnNCHFjM1d8g44fzBM0cV3eL';
-	} elseif (strpos($base, Curl_Request::local) !== false || strpos($base, Curl_Request::dev) !== false) {
-		$clientid = '625200931795-v9j07677ch7drvplu5ohpph4u48b4277.apps.googleusercontent.com';
-		$clientsecret = '9a-ThTIiKvuELANSjq-HFn4g';
-	} elseif (strpos($base, Curl_Request::production) !== false) {
-		$clientid = '87855736095-9bo8c78nojsne31dj3vr3gf694itavoh.apps.googleusercontent.com';
-		$clientsecret = 'DHXnd3vSUDQI8C0YOLmLk4YO';
-	}
+	$clientid = '347878416490-7era9p9mn379qdod4vbd61s217ubk2co.apps.googleusercontent.com';
+	$clientsecret = 'TnNCHFjM1d8g44fzBM0cV3eL';
 
 $config['google']['client_id']        = $clientid;
 $config['google']['client_secret']    = $clientsecret;
