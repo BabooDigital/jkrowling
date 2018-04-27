6ec0c99b40700b7f0297088bc63d30f561debba0
Aditia Nugraha <aditia@baboo.id>
2018-04-27 11:19:12 +0700

    Search if lenght key 3 and delay 1sec for searching

--- public/js/custom/search.js
+++ public/js/custom/search.js
@@ -14,98 +14,105 @@ $(document).ready(function () {
         //   '</form>');
         // $(boo).append(form);
         // form.submit();
-        console.log("aaa");
+        // console.log("aaa");
     });
-    $(document).on("keyup", "#searchss", function () {
+
+    $(document).on("keyup", "#searchss", function (event) {
         $("#myWorkContent").html("<div id='insideDiv'></div>");
         $("#search_books").html("");
         var bak = $(this).val();
-        // alert(bak);
-        var ab = $(this),
+        if (bak.length >= 3) {
+            var ab = $(this),
             b = new FormData;
-        b.append("search", $(this).val());
-        $.ajax({
-            url: base_url + "searching",
-            type: "POST",
-            dataType: "JSON",
-            cache: !1,
-            contentType: !1,
-            processData: !1,
-            data: b,
-            beforeSend: function() {
-                $('.loader').show();
-              }
-        }).done(function (data) {
-            if (Object.keys(data.user).length > 0) {
-                var userhtml = "";
+            b.append("search", $(this).val());
+            ab.data('timer', setTimeout(function () {
+                $.ajax({
+                    url: base_url + "searching",
+                    type: "POST",
+                    dataType: "JSON",
+                    cache: !1,
+                    contentType: !1,
+                    processData: !1,
+                    data: b,
+                    beforeSend: function() {
+                        $('.loader').show();
+                    }
+                }).done(function (data) {
+                    if (Object.keys(data.user).length > 0) {
+                        var userhtml = "";
 
+                        if (bak.length >= 1) {
+                            $.each(data.user, function (valkey, valid) {
+                                var follow;
+                                var txtfol;
+                                if (valid.isFollow == false) {
+                                    follow = 'follow-u';
+                                    txtfol = 'Follow';
+                                }else {
+                                    follow = 'unfollow-u';
+                                    txtfol = 'Unfollow';
+                                }
+                                var prof_picts = valid.prof_pict;
+                                if (valid.prof_pict == '' || valid.prof_pict == null) {
+                                    prof_picts = base_url + "public/img/profile/blank-photo.jpg";
+                                }
+                                userhtml += "<div class='card mr-10 pull-left' style='width:15%;'> <div class='container'> <div class='row'> <div class='col-12'> <div class='text-center p-10'> <img src='" + prof_picts + "' width='50' height='50' class='rounded-circle'> <p class='nametitled'><a href='"+base_url+"profile/"+valid.user_id+"-"+convertToSlug(valid.fullname)+"'>" + valid.fullname + "</a></p> </div> </div> </div> <div class='row'> <div class='col-6 text-center rborder'> <p style='display: inline-flex;'><img src='public/img/icon-tab/book.svg' width='25'> <span>" + valid.book_made + "</span></p> <h6>Buku</h6> </div> <div class='col-6 text-center'> <p style='display: inline-flex;'><img src='public/img/icon-tab/followers.svg' width='25'> <span>" + valid.followers + "</span></p> <h6>Teman</h6> </div> </div> <br> <div class='row'> <div class='col-12 text-center'> <button class='btnfollow-f "+follow+"' data-follow='" + valid.user_id + "'><img src='public/img/icon-tab/add_follow.svg' width='30'> <span class='txtfollow'>"+txtfol+"</span></button> </div> </div> <br> </div> </div>";
+                            });
+                        }
+                        $('.loader').hide();
+                        $("#insideDiv").html(userhtml);
+                    }
 
-                if (bak.length >= 1) {
-                    $.each(data.user, function (valkey, valid) {
-                        var follow;
-                        var txtfol;
-                        if (valid.isFollow == false) {
-                            follow = 'follow-u';
-                            txtfol = 'Follow';
-                        }else {
-                            follow = 'unfollow-u';
-                            txtfol = 'Unfollow';
-                        }
-                        var prof_picts = valid.prof_pict;
-                        if (valid.prof_pict == '' || valid.prof_pict == null) {
-                            prof_picts = base_url + "public/img/profile/blank-photo.jpg";
-                        }
-                        userhtml += "<div class='card mr-10 pull-left' style='width:15%;'> <div class='container'> <div class='row'> <div class='col-12'> <div class='text-center p-10'> <img src='" + prof_picts + "' width='50' height='50' class='rounded-circle'> <p class='nametitled'><a href='"+base_url+"profile/"+valid.user_id+"-"+convertToSlug(valid.fullname)+"'>" + valid.fullname + "</a></p> </div> </div> </div> <div class='row'> <div class='col-6 text-center rborder'> <p style='display: inline-flex;'><img src='public/img/icon-tab/book.svg' width='25'> <span>" + valid.book_made + "</span></p> <h6>Buku</h6> </div> <div class='col-6 text-center'> <p style='display: inline-flex;'><img src='public/img/icon-tab/followers.svg' width='25'> <span>" + valid.followers + "</span></p> <h6>Teman</h6> </div> </div> <br> <div class='row'> <div class='col-12 text-center'> <button class='btnfollow-f "+follow+"' data-follow='" + valid.user_id + "'><img src='public/img/icon-tab/add_follow.svg' width='30'> <span class='txtfollow'>"+txtfol+"</span></button> </div> </div> <br> </div> </div>";
-                    });
-                }
-                $('.loader').hide();
-                $("#insideDiv").html(userhtml);
-            }
+                    if(Object.keys(data.user).length == 0){
+                        var xhtml ='<div class="container"><h5 class="text-muted">Tidak ada pengguna yang dicari</h5><div>';
+                        $("#myWorkContent").html(xhtml);
+                    }
 
-            if(Object.keys(data.user).length == 0){
-                var xhtml ='<div class="container"><h5 class="text-muted">Tidak ada pengguna yang dicari</h5><div>';
-                $("#myWorkContent").html(xhtml);
-            }
+                    if (Object.keys(data.books).length > 0) {
+                        var bookhtml = "";
+                        if (bak.length >= 1) {
+                            $.each(data.books, function (valkay, valad) {
+                                var ava = valad.author_avatar;
+                                if (valad.author_avatar == null || valad.author_avatar == '') {
+                                    ava = 'public/img/profile/blank-photo.jpg';
+                                }
+                                var cover_url = valad.cover_url;
+                                if (valad.cover_url == '' || valad.cover_url == null) {
+                                    cover_url = "https://assets.dev-baboo.co.id/baboo-cover/default1.png";
+                                }
+                                var image_url = valad.image_url;
+                                if (valad.image_url == '' || valad.image_url == null) {
+                                    image_url = cover_url;
+                                }
+                                var unlikes = "like";
+                                var img_love = base_url + "public/img/assets/icon_love.svg";
+                                if(valad.is_like == true){
+                                    unlikes = "unlike";
+                                    img_love = base_url + "public/img/assets/love_active.svg";
+                                }
+                                bookhtml += "<div class='card mb-15 p-0 text-left'> <div class='card-body p-0 pl-30 pr-30 pt-15'> <div class='row mb-10 pl-15 pr-15'> <div class='media'> <img class='d-flex align-self-start mr-20 rounded-circle' src='"+ ava +"' width='50' height='50' alt='"+ valad.author_name +"'> <div class='media-body mt-5'> <h5 class='card-title nametitle2'><a href='"+base_url+"profile/"+ valad.author_id +"-"+convertToSlug(valad.author_name)+"' class='author_name'>"+ valad.author_name +"</a></h5> <p class='text-muted' style='margin-top:-10px;'><small><span>"+ valad.publish_date +"</span></small></p> </div> </div> </div> <div class='media'> <a href='book/"+ valad.book_id+"-"+convertToSlug(valad.title_book) +"'><img alt='"+valad.title_book+"' src='"+cover_url+"' class='w-100 imgcover cover_image'></a> </div> <a href='book/"+ valad.book_id+"-"+convertToSlug(valad.title_book) +"' class='segment' data-href='"+ valad.book_id+"'><h5 class='pt-20 w-100' style='font-weight: 700;'><b class='dbooktitle'>"+valad.title_book+"</b></h5></a> <div class='w-100'> <span class='mr-8' style='font-size: 12px;'>"+valad.category+" &#8226;</span> <span class='text-muted' style='font-size: 11px;'>Dibaca "+ valad.view_count +" kali</span> <p class='mt-10 textp' data-text='"+ valad.desc +"'>"+ valad.desc.substr(0, 100)+'...' +"</p> </div> </div> <div class='bg-white card-footer text-muted pr-30 pl-30' style='font-size: 0.8em;font-weight: bold;'> <div class='pull-right'> <div class='dropdown'> <button class='share-btn dropbtn' type='button' id='dropShare' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> <img src='public/img/assets/icon_share.svg' width='23'> </button> <div class='dropdown-menu' aria-labelledby='dropShare'> <a class='dropdown-item share-fb' href='javascript:void(0);' data-share='"+valad.book_id+"'><img src='public/img/assets/fb-icon.svg' width='20'> Facebook</a> </div> </div> </div> <div> <a data-id='"+ valad.book_id+"' href='javascript:void(0);' class='"+unlikes+"'><img src='"+img_love+"' class='mr-20 loveicon' width='27'></a> <a href='book/"+ valad.book_id+"-"+convertToSlug(valad.title_book) +"#comments'><img src='public/img/assets/icon_comment.svg' class='mr-10' width='25'></a> </div> </div> </div>";
+                            });
+                        }
+                        $('.loader').hide();
+                        $("#search_books").html(bookhtml);
+                    }
 
-            if (Object.keys(data.books).length > 0) {
-                var bookhtml = "";
-                if (bak.length >= 1) {
-                    $.each(data.books, function (valkay, valad) {
-                        var ava = valad.author_avatar;
-                        if (valad.author_avatar == null || valad.author_avatar == '') {
-                            ava = 'public/img/profile/blank-photo.jpg';
-                        }
-                        var cover_url = valad.cover_url;
-                        if (valad.cover_url == '' || valad.cover_url == null) {
-                            cover_url = "https://assets.dev-baboo.co.id/baboo-cover/default1.png";
-                        }
-                        var image_url = valad.image_url;
-                        if (valad.image_url == '' || valad.image_url == null) {
-                            image_url = cover_url;
-                        }
-                        var unlikes = "like";
-                        var img_love = base_url + "public/img/assets/icon_love.svg";
-                        if(valad.is_like == true){
-                            unlikes = "unlike";
-                            img_love = base_url + "public/img/assets/love_active.svg";
-                        }
-                        bookhtml += "<div class='card mb-15 p-0 text-left'> <div class='card-body p-0 pl-30 pr-30 pt-15'> <div class='row mb-10 pl-15 pr-15'> <div class='media'> <img class='d-flex align-self-start mr-20 rounded-circle' src='"+ ava +"' width='50' height='50' alt='"+ valad.author_name +"'> <div class='media-body mt-5'> <h5 class='card-title nametitle2'><a href='"+base_url+"profile/"+ valad.author_id +"-"+convertToSlug(valad.author_name)+"' class='author_name'>"+ valad.author_name +"</a></h5> <p class='text-muted' style='margin-top:-10px;'><small><span>"+ valad.publish_date +"</span></small></p> </div> </div> </div> <div class='media'> <a href='book/"+ valad.book_id+"-"+convertToSlug(valad.title_book) +"'><img alt='"+valad.title_book+"' src='"+cover_url+"' class='w-100 imgcover cover_image'></a> </div> <a href='book/"+ valad.book_id+"-"+convertToSlug(valad.title_book) +"' class='segment' data-href='"+ valad.book_id+"'><h5 class='pt-20 w-100' style='font-weight: 700;'><b class='dbooktitle'>"+valad.title_book+"</b></h5></a> <div class='w-100'> <span class='mr-8' style='font-size: 12px;'>"+valad.category+" &#8226;</span> <span class='text-muted' style='font-size: 11px;'>Dibaca "+ valad.view_count +" kali</span> <p class='mt-10 textp' data-text='"+ valad.desc +"'>"+ valad.desc.substr(0, 100)+'...' +"</p> </div> </div> <div class='bg-white card-footer text-muted pr-30 pl-30' style='font-size: 0.8em;font-weight: bold;'> <div class='pull-right'> <div class='dropdown'> <button class='share-btn dropbtn' type='button' id='dropShare' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> <img src='public/img/assets/icon_share.svg' width='23'> </button> <div class='dropdown-menu' aria-labelledby='dropShare'> <a class='dropdown-item share-fb' href='javascript:void(0);' data-share='"+valad.book_id+"'><img src='public/img/assets/fb-icon.svg' width='20'> Facebook</a> </div> </div> </div> <div> <a data-id='"+ valad.book_id+"' href='javascript:void(0);' class='"+unlikes+"'><img src='"+img_love+"' class='mr-20 loveicon' width='27'></a> <a href='book/"+ valad.book_id+"-"+convertToSlug(valad.title_book) +"#comments'><img src='public/img/assets/icon_comment.svg' class='mr-10' width='25'></a> </div> </div> </div>";
-                    });
-                }
-                $('.loader').hide();
-                $("#search_books").html(bookhtml);
-            }
-
-            if(Object.keys(data.books).length == 0){
-                var bhtml ='<div class="container"><h5 class="text-muted">Tidak ada buku yang dicari</h5><div>';
-                $('.loader').hide();
-                $("#search_books").html(bhtml);
-            }
-        }).fail(function () {
-            console.log("error")
-        }).always(function () {
-        })
-    });
+                    if(Object.keys(data.books).length == 0){
+                        var bhtml ='<div class="container"><h5 class="text-muted">Tidak ada buku yang dicari</h5><div>';
+                        $('.loader').hide();
+                        $("#search_books").html(bhtml);
+                    }
+                }).fail(function () {
+                    console.log("error")
+                }).always(function () {
+                });
+            }, 1000));
+    };
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+});
 
 $(document).on("click", ".like", function() {
         var b = $(this),
@@ -231,8 +238,7 @@ function convertSearch(Text) {
             data: formdata,
         })
         .done(function(data) {
-            if (search.length >=0) {
-                console.log(data['books']);
+            if (search.length >=3) {
                 $(".search_result_bbo").addClass('show');
                 var list_book = '',
                     list_user = '';
@@ -253,7 +259,6 @@ function convertSearch(Text) {
             console.log("error");
         })
         .always(function() {
-            console.log("complete");
         });
     });
 
