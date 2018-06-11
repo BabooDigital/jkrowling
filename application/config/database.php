9a702d4702a4afbb5739a0fb9b0b6f87d50c3e43
paulsenjs <paulsenjs5@gmail.com>
2018-06-11 15:42:13 +0700

    update config and delete unused code

--- application/config/database.php
+++ application/config/database.php
@@ -71,24 +71,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-$db['default'] = array(
-    'dsn' => '',
-    'hostname' => 'dbnode0',
-    'username' => 'baboo_dbusr',
-    'password' => 'B4booAWs2018',
-    'database' => 'baboo_dbstg',
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
+$db['default'] = array();
